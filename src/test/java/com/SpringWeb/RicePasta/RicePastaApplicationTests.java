b90842217a26aa606e9bbb6196a9e78ff7e94502
HONG0805 <minjee096818@gmail.com>
2023-01-10 22:35:46 +0900

    Initial commit

---
+++ src/test/java/com/SpringWeb/RicePasta/RicePastaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringWeb.RicePasta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RicePastaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
