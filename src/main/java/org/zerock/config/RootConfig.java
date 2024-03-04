c8771a4d0066597ee79be72c7834233b5d6b30bc
hong0805 <minjee096818@gmail.com>
2024-03-04 22:12:05 +0900

    sp1

--- src/main/java/org/zerock/config/RootConfig.java
+++ src/main/java/org/zerock/config/RootConfig.java
@@ -7,12 +7,14 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
 @Configuration
-@MapperScan(basePackages = {"org.zerock.mapper"})
+@ComponentScan(basePackages = "org.zerock.service")
+@MapperScan(basePackages = { "org.zerock.mapper" })
 public class RootConfig {
 
 	@Bean
@@ -28,9 +30,9 @@ public class RootConfig {
 		return dataSource;
 
 	}
-	
+
 	@Bean
-	public SqlSessionFactory sqlSessionFactory() throws Exception{
+	public SqlSessionFactory sqlSessionFactory() throws Exception {
 		SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
 		sqlSessionFactory.setDataSource(dataSource());
 		return (SqlSessionFactory) sqlSessionFactory.getObject();
--- src/main/java/org/zerock/config/WebConfig.java
+++ src/main/java/org/zerock/config/WebConfig.java
@@ -1,28 +1,39 @@
 package org.zerock.config;
 
+import javax.servlet.Filter;
 import javax.servlet.ServletRegistration;
 
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
-
+	
 	@Override
-	protected Class<?>[] getRootConfigClasses() {
-		return new Class[] { RootConfig.class };
+	protected Filter[] getServletFilters() {
+		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+		characterEncodingFilter.setEncoding("UTF-8");
+		characterEncodingFilter.setForceEncoding(true);
+		
+		return new Filter[] { characterEncodingFilter };
 	}
 
-	@Override
-	protected Class<?>[] getServletConfigClasses() {
-		return new Class[] { ServletConfig.class };
-	}
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] { RootConfig.class };
+    }
 
-	@Override
-	protected String[] getServletMappings() {
-		return new String[] { "/" };
-	}
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] { ServletConfig.class };
+    }
 
-	@Override
-	protected void customizeRegistration(ServletRegistration.Dynamic registration) {
-		registration.setInitParameter("throwExceptionIfNotHandlerFound", "true");
-	}
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
+
+    @Override
+    protected void customizeRegistration(ServletRegistration.Dynamic registration) {
+        registration.setInitParameter("throwExceptionIfNotHandlerFound", "true");
+    }
 }
